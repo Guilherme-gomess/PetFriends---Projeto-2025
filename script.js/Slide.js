5457fb3021171a59f64e46178e7a5e2ea7d00937
Camilla Vitoria <camillav09@gmail.com>
2025-06-04 13:32:42 -0300

    Arquivos do projeto.

--- script.js/Slide.js
+++ script.js/Slide.js
@@ -3,14 +3,14 @@ document.getElementById("radio1").checked = true;
 
 setInterval( function() {
     nextImage();
-}, 5000)
+}, 4000)
 
 function nextImage () {
     count++;
     if (count > 4) {
         count = 1;
-}
-
+    }
+    
 document.getElementById("radio"+count).checked = true;
 
 }
